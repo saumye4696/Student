12944edd60ec5053dfc716df94a8d65d7e26c303
saumye4696 <saumye.mehrotra@gmail.com>
2022-03-02 01:11:06 +0530

    Initial commit

---
+++ src/main/java/com/demo/student/model/Student.java
@@ -0,0 +1,34 @@
+package com.demo.student.model;
+
+import javax.persistence.Table;
+
+@Table
+public class Student {
+    private String name;
+    private String id;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
